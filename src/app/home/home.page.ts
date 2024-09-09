d522357e817733ba503b3720e5fa8a7d1fcce606
Emelly <125489310+Mellys116@users.noreply.github.com>
2024-09-09 10:52:24 +0000

    alterção

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
+
+
 export class HomePage {
-  constructor() {}
+ public gasolina: number | undefined;
+ public alcool: number | undefined;
+ public calculo: number | undefined;
+ public resultado: string = "";
+ constructor(private alertController: AlertController) {}
+
+
+ async Comparar() {
+ if (this.gasolina == null || this.alcool == null) {
+ const alert = await this.alertController.create({
+ header: 'Atenção',
+ message: 'Por favor, digite os dois valores',
+ buttons: ['OK']
+ });
+ await alert.present();
+ return;
+ }
+ 
+this.calculo = this.alcool / this.gasolina;
+console.log(this.calculo);
+
+
+if (this.calculo <= 0.7){
+  this.resultado = "Alcool é mais vantajoso :3";
+}else{
+  this.resultado = "Gasolina é mais vantajoso :0";
+}
+console.log('Resultado', this.resultado);
+}
 }
